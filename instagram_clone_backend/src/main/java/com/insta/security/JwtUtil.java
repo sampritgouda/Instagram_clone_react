92ce58d6994f11d4d5ddcc4e547adff2b3ab58d0
sampritgouda <goudasamprit@gmail.com>
2026-08-20 01:18:59 +0530

    added online and double tap for like isssue for deploy fixed

--- instagram_clone_backend/src/main/java/com/insta/security/JwtUtil.java
+++ instagram_clone_backend/src/main/java/com/insta/security/JwtUtil.java
@@ -5,7 +5,7 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.security.Key;
 import java.util.Date;
 
